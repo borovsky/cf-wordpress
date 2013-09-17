4db0c3bcb82efcb48f06b9deb8303e376eb88998
Alexander Borovsky <alex.borovsky@gmail.com>
2013-09-16 18:43:41 -0700

    Base commit

---
+++ wp-salt.php
@@ -0,0 +1,9 @@
+<?php
+define('AUTH_KEY',         '$EUtc--XXToE?&(R0]_OAf`o5(||gg6>f@73VRT76eG<.QIGg^yEt$xO/nZ}So+_');
+define('SECURE_AUTH_KEY',  'qA!j[X{n^Imt^7=-;+lwFoj+ywa?y0+`>>xw7c.gDk8dwM-$}e9c3Vl0MRBNU+yi');
+define('LOGGED_IN_KEY',    'i=&09-QITgO,A-m[doc/xSO !^[+%TJ[h$PPb*oxQJ{S|6S.m$(1*+/$?(.^sRAn');
+define('NONCE_KEY',        '|OA,64z7a8+{[j8wTO_.hP/{ML@<P$%l=grs8DM:Xn4xYuEt 0pnzSV2D;GzBQ8*');
+define('AUTH_SALT',        'wOu!L|O0(QfMF=>d{@DYl)-Ogd+!Qg=EK+n=R[[9v%I6esa1sHIUi!i6lB] ?->u');
+define('SECURE_AUTH_SALT', '3Vg8yuUEb|d}m|?fyv%s1UOD+S94-A!z +1yQQAR-6W3riMR+a,&y|kE+I`Y?@l+');
+define('LOGGED_IN_SALT',   '~/C)PbJw_)nH#9l&x9]Z6&7iSg|>[+ZhA}fGE=>vvqVw#m0e{or<i=eA$e{B^)jD');
+define('NONCE_SALT',       '&svTq*{~)$0:VLILH+fBw1;@<tO&?l`y?p-TY{-5DT@7Q~{8[)0li|c-I)C#FrP6');
